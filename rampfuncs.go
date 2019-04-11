9216de9922d344021e276320e277df33d5197a44
idk <idk@idk.local>
2019-04-11 18:42:51 -0400

    functional constructor for NewSAM

--- rampfuncs.go
+++ rampfuncs.go
@@ -1,21 +1,21 @@
 package ramp
 
-func (f Ramp) sam() string {
+func (f *Ramp) sam() string {
 	return f.SAM.Config.I2PConfig.Sam()
 }
 
-func (f Ramp) leasesetsettings() (string, string, string) {
+func (f *Ramp) leasesetsettings() (string, string, string) {
 	return f.SAM.Config.I2PConfig.Leasesetsettings()
 }
 
-func (f Ramp) print() []string {
+func (f *Ramp) print() []string {
 	return f.SAM.Config.I2PConfig.Print()
 }
 
-func (f Ramp) accesslisttype() string {
+func (f *Ramp) accesslisttype() string {
 	return f.SAM.Config.I2PConfig.Accesslisttype()
 }
 
-func (f Ramp) accesslist() string {
+func (f *Ramp) accesslist() string {
 	return f.SAM.Config.I2PConfig.Accesslist()
 }
